7761e3637c2ea8c8a6fcfe42e8414d530099a585
Barry1701 <>
2023-12-11 17:16:28 +0000

    Add initial commit to my hangman project

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+no_of_tries = 5
+word = "bartosz"
+
+while True:
+    letter = input("Podaj Litere: ")
